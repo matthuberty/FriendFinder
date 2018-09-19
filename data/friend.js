b659cc230129072b47ab9953da2fd7dc7db18cea
Matthew Huberty <matthew.huberty@gmail.com>
2018-09-19 10:01:56 -0700

    Sept19_v2

---
+++ data/friend.js
@@ -0,0 +1,25 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize array with starter data
+
+var friendList = [
+    {
+      name: 'Matthew J. Huberty',
+      photo: 'https://media.licdn.com/dms/image/C5603AQFdop4OPjW0mw/profile-displayphoto-shrink_200_200/0?e=1542844800&v=beta&t=HmslOruSV-2c-4R5x9bB_PnGsF9JaqVLRgDC5smSovI',
+      scores: [
+        "3",
+        "3",
+        "2",
+        "2",
+        "3",
+        "4",
+        "4",
+        "2",
+        "2",
+        "2"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
+
+  //https://www.linkedin.com/in/matthew-huberty-8488252
